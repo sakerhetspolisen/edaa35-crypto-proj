8d2b14426e51af19189991287604363725f2d6b3
Karl F. Sellergren <68159964+sakerhetspolisen@users.noreply.github.com>
2024-05-07 15:39:00 +0200

    Initial commit

---
+++ src/sorters/Labb.java
@@ -0,0 +1,88 @@
+package sorters;
+
+import javax.crypto.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.LinkedList;
+
+public class Labb {
+
+    static LinkedList<Integer> copiedList;
+    static String input;
+    static String algorithm;
+    static SecretKey key;
+    static CryptoMethods crypto;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        crypto = new CryptoMethods();
+        algorithm = args[0];
+        Integer keyLen = Integer.parseInt(args[1]);
+        String outFile = args[2];
+        Integer iterations = Integer.parseInt(args[3]);
+        input = generateRandomString(algorithm == "AES" ? 128 : 64);
+        key = generateKey(keyLen, algorithm);
+
+        LinkedList<Long> evaluationTimes = null;
+        try {
+            evaluationTimes = getEvaluationTimes(iterations);
+        } catch (InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+        writeLinkedListToFile(outFile, evaluationTimes);
+    }
+
+    public static String generateRandomString(int byteSize) {
+        byte[] randomBytes = new byte[byteSize];
+        new SecureRandom().nextBytes(randomBytes);
+        return new String(randomBytes, StandardCharsets.UTF_8);
+    }
+
+    private static LinkedList<Long> getEvaluationTimes(int iterations) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        LinkedList<Long> evaluationTimes = new LinkedList<>();
+        long startTime;
+        long endTime;
+
+        for (int i = 0; i < iterations; i++) {
+            startTime = System.nanoTime();
+            crypto.encrypt(algorithm, input, key);
+            endTime = System.nanoTime();
+            evaluationTimes.add(endTime-startTime);
+            copiedList = null;
+        }
+
+        return evaluationTimes;
+    }
+
+    private static <E> void writeLinkedListToFile(String fileName, LinkedList<E> list) {
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write("Iteration" + " " + "Time" + "\n");
+            for (int i = 1; i < list.size() + 1; i++) {
+                writer.write(i + " " + list.get(i - 1) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Couldn't write to outfile");
+        }
+    }
+
+    public static SecretKey generateKey(int n, String algorithm) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(algorithm);
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+}
